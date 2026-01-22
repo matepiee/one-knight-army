6bc12b2c7ce2ffaaa3e01f08bf25b1b3bcb7ba55
mark0956 <juhaszmark60@gmail.com>
2026-01-22 19:48:08 +0100

    Shop done except selling is not working

--- Assets/Scripts/Inventory&Shop/Loot.cs
+++ Assets/Scripts/Inventory&Shop/Loot.cs
@@ -9,28 +9,52 @@ public class Loot : MonoBehaviour
     public SpriteRenderer sr;
     public Animator anim;
 
+    public bool canBePickedUp = true;
     public int quantity;
     public static event Action<ItemSO, int> OnItemLooted;
 
     private void OnValidate()
     {
-        if(itemSO == null)
+        if (itemSO == null)
         {
             return;
         }
+        UpdateAppearance();
+    }
+
+
+    public void Initialize(ItemSO itemSO, int quantity)
+    {
+        this.itemSO = itemSO;
+        this.quantity = quantity;
+        canBePickedUp = false;
+
+        UpdateAppearance();
+    }
+
+    private void UpdateAppearance()
+    {
         sr.sprite = itemSO.icon;
         this.name = itemSO.name;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && canBePickedUp == true)
         {
             GetComponent<Collider2D>().enabled = false;
 
             anim.Play("LootPickup");
-            OnItemLooted?.Invoke(itemSO,quantity);
+            OnItemLooted?.Invoke(itemSO, quantity);
             Destroy(gameObject, .5f);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            canBePickedUp = true;
         }
     }
 }
---
+++ Assets/Scripts/Inventory&Shop/Shop/ShopSlot.cs
@@ -0,0 +1,30 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ShopSlot : MonoBehaviour
+{
+    public ItemSO itemSO;
+    public TMP_Text itemNameText;
+    public TMP_Text priceText;
+    public Image itemImage;
+
+    [SerializeField] private ShopManager shopManager;
+
+    public int price;
+    public void Initialize(ItemSO newItemSO, int price)
+    {
+        itemSO = newItemSO;
+        itemImage.sprite = itemSO.icon;
+        itemNameText.text = itemSO.itemName;
+        this.price = price;
+        priceText.text = price.ToString();
+    }
+
+    public void OnBuyButtonClicked()
+    {
+        shopManager.TryBuyItem(itemSO, price);
+    }
+}
