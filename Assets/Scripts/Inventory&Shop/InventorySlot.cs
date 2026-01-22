68e45132704f9ff9582d06633f4c8621cbeb3843
mark0956 <juhaszmark60@gmail.com>
2026-01-22 15:46:28 +0100

    Error fixed, inventory working

--- Assets/Scripts/Inventory&Shop/InventorySlot.cs
+++ Assets/Scripts/Inventory&Shop/InventorySlot.cs
@@ -1,14 +1,34 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
 
-public class InventorySlot : MonoBehaviour
+public class InventorySlot : MonoBehaviour, IPointerClickHandler
 {
     public ItemSO itemSO;
     public int quantity;
 
     public Image itemImage;
     public TMP_Text quantityText;
+
+    public InventoryManager inventoryManager;
+
+    private void Start()
+    {
+        inventoryManager = GetComponent<InventoryManager>();        
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(quantity > 0)
+        {
+            if(eventData.button == PointerEventData.InputButton.Left)
+            {
+                inventoryManager.UseItem(this);
+            }
+        }
+    }
 
     public void UpdateUI()
     {
