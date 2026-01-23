9bda4cd7d84a88e0e311f8795bc4ab2f6219a97a
Bacskai Bence <bacskaibence2441@PETOFI-ASZOD.LAN>
2026-01-23 08:50:56 +0100

    Enemy and Player animations added.

--- Assets/Scripts/Enemy_Combat.cs
+++ Assets/Scripts/Enemy_Combat.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 public class Enemy_Combat : MonoBehaviour
 {
     public int damage = 10;
-    
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        collision.gameObject.GetComponent<Player_Health>().ChangeHealth(-damage);
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Player_Health>().ChangeHealth(-damage);
+        }
+
     }
 
 }
